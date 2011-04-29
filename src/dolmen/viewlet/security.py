efa3e6dd8c2bc7df3f96a0b300ef53d97b5981f6
Souheil CHELFOUH <trollfot@gmail.com>
2011-04-29 16:28:22 +0200

    Added security grokking components

---
+++ src/dolmen/viewlet/security.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+try:
+    import martian
+    import grokcore.security
+    from dolmen import viewlet
+    from cromlech.browser.interfaces import IRenderer
+
+    class ViewSecurityGrokker(martian.ClassGrokker):
+        martian.component(viewlet.Viewlet)
+        martian.directive(grokcore.security.require, name='permission')
+
+        def execute(self, factory, config, permission, **kw):
+            # we can also check here for ISecuredItem
+            for method_name in IRenderer:
+                config.action(
+                    discriminator=('protectName', factory, method_name),
+                    callable=grokcore.security.util.protect_getattr,
+                    args=(factory, method_name, permission),
+                    )
+            return True
+
+except ImportError:
+    pass
